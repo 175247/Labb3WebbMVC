0ecc901932f094925d19fdce1ca54b7a4b6cbc12
175247 <yarr-tm@hotmail.com>
2020-03-29 13:06:03 +0200

    Initial commit

---
+++ Labb3WebbMVC/Models/CinemaContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Labb3WebbMVC.Models
+{
+    public class CinemaContext : DbContext
+    {
+        public DbSet<Movie> MovieList { get; set; }
+        public DbSet<Salon> SalonList { get; set; }
+
+        public CinemaContext(DbContextOptions<CinemaContext> options)
+            : base(options)
+        {
+
+        }
+    }
+}
---
+++ Labb3WebbMVC/Models/Movie.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Labb3WebbMVC.Models
+{
+    public class Movie
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public double Duration { get; set; }
+        public DateTime StartingTime { get; set; }
+        public Salon Salon { get; set; }
+    }
+}
